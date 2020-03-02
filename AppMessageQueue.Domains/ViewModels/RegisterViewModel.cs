00f178ab1d72f0821f6cd0ce949cf2ed8dfe1de4
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-03-02 00:39:25 +0000

    Updated component model

--- AppMessageQueue.Domains/ViewModels/RegisterViewModel.cs
+++ AppMessageQueue.Domains/ViewModels/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,20 @@ namespace AppMessageQueue.Domains.ViewModels
 {
     public class RegisterViewModel
     {
-        public int Id { get; internal set; }
-        public string EmailAddress { get; internal set; }
-        public Guid ApplicationInstance { get; internal set; }
+        public int Id { get; set; }
+
+        [Required, EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public Guid ApplicationInstance { get; set; }
     }
 }
